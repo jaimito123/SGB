9549633cb33ace3af5cfbd8bcf6063d332a623bf
jaimito <jaimitoluciochrisbel@gmail.com>
2018-07-03 14:00:24 +0200

    teste pessoal

--- src/test/java/servicos/TesteListaObra.java
+++ src/test/java/servicos/TesteListaObra.java
@@ -5,12 +5,29 @@
  */
 package servicos;
 
+import conexao.JPA;
+import controladores.entidades.SgObraJpaController;
+import entidades.SgObra;
 import junit.framework.TestCase;
+import org.zkoss.zul.ListModelList;
 
 /**
  *
  * @author Lucio
  */
 public class TesteListaObra extends TestCase{
-    
+    public void teste_1(){
+        ListModelList<SgObra> books = new ListModelList<SgObra>(new SgObraJpaController(new JPA().getEmf()).findSgObraEntities("Informática"));
+        assertEquals(books.getSize(), 7);
+    }
+    public void teste_2(){
+        ListModelList<SgObra> books = new ListModelList<SgObra>(new SgObraJpaController(new JPA().getEmf()).findSgObraEntities("Informática"));
+        assertEquals(books.get(0).getTitulo(), "Sistemas Distribuidos : Conceitos e Projectos");
+        assertEquals(books.get(1).getTitulo(), "Engenharia de Software : Uma Abordagem Professional");
+    }
+    public void teste_3(){
+        ListModelList<SgObra> books = new ListModelList<SgObra>(new SgObraJpaController(new JPA().getEmf()).findSgObraEntities("Ciências Biológicas"));
+        assertEquals(books.size(), 0);
+        
+    }
 }
